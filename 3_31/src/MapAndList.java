b2ec3260d9c364909408f5cfb42788fbcb9e147e
16633445526@163.com <16633445526@163.com>
2025-03-31 21:06:29 +0800

    second commit

---
+++ 3_31/src/MapAndList.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapAndList {
+    public static void main(String[]args) {
+        Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+        map.put(1, 2);
+        map.put(2, 1);
+        map.put(3, 100);
+        int x=map.get(1);
+        int y=map.get(2);
+        //size()返回key-value 组数
+        int z=map.size();
+        int k=map.getOrDefault(3, -1);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(k);
+    }
+    }
+
+
